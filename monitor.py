cb6441b9022cc55db1d57df8d09f50b11bbc4516
Burak Gunes <burakemrullahgunes@gmail.com>
2022-05-24 16:17:53 +0300

    initial commit

---
+++ monitor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import psutil
+import platform
+import cpuinfo
+
+def get_uptime():
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+
+    return uptime_seconds
+
+
+print('CPU',cpuinfo.get_cpu_info()["brand"],'Platform :', platform.processor(),'Uptime :', get_uptime())
+
+while True:
+   cpu=psutil.cpu_percent(interval=1)
+   ram=psutil.virtual_memory().percent
+   avail_ram=psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
+   disk= psutil.disk_usage('/')
+   time=psutil.boot_time()
+   print('CPU ',cpu, 'MEM ',ram, 'AVAIL', avail_ram, 'DISK ' , disk)
+
+
